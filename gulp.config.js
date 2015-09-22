4fb6623e9c6f780a88aa225b5b91637cbd5c6bbe
Jukka Arola <jukka.arola@gmail.com>
2015-09-22 20:53:18 +0300

    cleaner gulp config
    separated paths to variables

--- gulp.config.js
+++ gulp.config.js
@@ -1,10 +1,31 @@
 module.exports = function() {
+
+    var rootDir = '.';
+
+    var sourceDir = rootDir + '/src';
+
+    var clientDir = sourceDir + '/client';
+
+    var serverDir = sourceDir + '/server';
+
     var config = {
+
+        /*
+         * file paths
+         */
+
+        // dir for temporary files
+        temp: rootDir + '/.temp',
 
         // all js-files we want to check for styling issues
         jsfiles: [
-            './src/**/*.js',
-            './*.js'
+            sourceDir + '/**/*.js',
+            rootDir + '/*.js'
+        ],
+
+        // all less files to compile into css etc.
+        less: [
+            clientDir + '/styles/*.less'
         ]
     };
 
